e539f67873ac10998202477726436a214188ed93
Johan Wallin <wallinnr12@gmail.com>
2018-11-12 20:03:14 +0100

    Initial Commit

---
+++ Swift app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift app
+//
+//  Created by Johan on 2018-11-12.
+//  Copyright © 2018 Johan W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
